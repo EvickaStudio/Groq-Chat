a74916785b4b9d6bff5865f03797fb0209de8780
EvickaStudio <kirenogfx@gmail.com>
2024-02-19 14:20:43 +0100

    Update Groq import and add stream response example

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from auth.JWT import Token
-from text_completion.completion import Groq
 
+# Normal mode
+from text_completion.completion import Groq as gq
+
+# Get Token
 token = Token().get_token()
 print(token)
 
-groq = Groq(token)
+# Initialize Groq with token
+groq = gq(token)
 
 response = groq.complete("What is the capital of France?")
 
 print(response)
+
+
+
+# Stream response example
+from text_completion.completion_stream import Groq as gqst
+
+# Get Token
+token = Token().get_token()
+
+# Initialize Groq with token
+groq = gqst(token)
+
+response = groq.complete("What is the capital of France?")
